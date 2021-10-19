1326746f503b43dad9580866f185bea32cbc6b4a
careful-sheep <1092979868@qq.com>
2021-10-19 12:56:00 +0800

    prettier+eslint+stylelint配置完毕

---
+++ .stylelintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // Positioning, Box Model, Typography, Visual, Animation, Misc
+    extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
+    plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
+    rules: {
+        'plugin/declaration-block-no-ignored-properties': true,
+        'comment-empty-line-before': 'always',
+        'declaration-empty-line-before': null,
+        'function-name-case': 'lower',
+        'no-descending-specificity': null,
+        'no-invalid-double-slash-comments': null,
+    },
+    ignoreFiles: ['node_modules/**/*', 'build/**/*'],
+};
